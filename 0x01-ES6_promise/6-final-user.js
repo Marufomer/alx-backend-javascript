541cc4c06a5364a88a35e5d4dd86e6253c0a6ab7
Marufomer <omermaruf07@gmail.com>
2024-10-02 09:52:13 +0000

    my promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => [{ status: 200, value: res }])
-    .catch((err) => [{ status: 401, value: err }]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((el) => ({
+        status: el.status,
+        value: el.status === 'fulfilled' ? el.value : String(el.Error),
+      }))
+    ));
 }
