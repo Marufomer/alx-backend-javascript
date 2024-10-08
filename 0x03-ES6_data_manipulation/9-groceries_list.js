8492f887a43f5dedaa31e7fafa9117c746354a2f
Marufomer <omermaruf07@gmail.com>
2024-10-08 14:00:27 +0000

    task-9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const grocerItems = new Map();
+  grocerItems.set('Apples', 10);
+  grocerItems.set('Tomatoes', 10);
+  grocerItems.set('Pasta', 1);
+  grocerItems.set('Rice', 1);
+  grocerItems.set('Banana', 5);
+  return grocerItems;
+}
